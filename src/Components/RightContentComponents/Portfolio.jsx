940f0f68ac5fa7e5597f84d2841d9cf52beaa2ae
Adrian Palcic <adrian.palcic@gmail.com>
2024-11-07 16:49:36 +0100

    asdasdasd

--- src/Components/RightContentComponents/Portfolio.jsx
+++ src/Components/RightContentComponents/Portfolio.jsx
@@ -3,32 +3,28 @@ import ProjectCard from "../projectCard/ProjectCard"
 import { IoLogoFirebase } from "react-icons/io5"
 import { SiRedux } from "react-icons/si"
 
-
-
 const Portfolio = () => {
-
-
     const projects = [
         {
             id: 1,
             title: "Virtual Caffe",
             desc: "A virtual caffe app used to keep track of tables and their orders. Simplifies work by removing the need to memorize orders. ",
-            techStack: [<FaReact className="tech-react" size="40px" />, <FaCss3Alt className="tech-css" size="40px" />, <SiRedux className="tech-redux" size="40px" />],
-            img: 'comein.jpg',
+            techStack: [<FaReact key="react" className="tech-react" size="40px" />, <FaCss3Alt key="css" className="tech-css" size="40px" />, <SiRedux key="redux" className="tech-redux" size="40px" />],
+            img: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/comein.jpg-pHPNwJwWt2zV01yk9JpSls5a7Hf7o4.jpeg"
         },
         {
             id: 2,
             title: "Instagram Clone",
             desc: "Instagram clone app. It's main functionallity was to teach me fullstack development using Firebase",
-            techStack: [<FaReact className="tech-react" size="40px" />, <FaCss3Alt className="tech-css" size="40px" />, <IoLogoFirebase className="tech-firebase" size="40px" />, <SiRedux className="tech-redux" size="40px" />],
-            img: 'Instaclone.jpg',
+            techStack: [<FaReact key="react" className="tech-react" size="40px" />, <FaCss3Alt key="css" className="tech-css" size="40px" />, <IoLogoFirebase key="firebase" className="tech-firebase" size="40px" />, <SiRedux key="redux" className="tech-redux" size="40px" />],
+            img: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Instaclone.jpg-HhYpNKSRDnUvAXUfPzA79Z3tz1NV6S.jpeg"
         },
         {
             id: 3,
             title: "E-commerce website",
             desc: "Classic E-commerce website with a focus on good design, responsive design, SEO optimization and user authentication",
-            techStack: [<FaReact className="tech-react" size="40px" />, <FaCss3Alt className="tech-css" size="40px" />, <IoLogoFirebase className="tech-firebase" size="40px" />, <SiRedux className="tech-redux" size="40px" />],
-            img: 'Ecomm.jpg',
+            techStack: [<FaReact key="react" className="tech-react" size="40px" />, <FaCss3Alt key="css" className="tech-css" size="40px" />, <IoLogoFirebase key="firebase" className="tech-firebase" size="40px" />, <SiRedux key="redux" className="tech-redux" size="40px" />],
+            img: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Ecomm.jpg-fcq6qfmURtwflqAmpJya9hKcF2gdq9.jpeg"
         },
     ]
 
